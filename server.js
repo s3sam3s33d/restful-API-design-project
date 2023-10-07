53b1e5fe9e77f3437178dfd2b2a8b0cb61401852
unknown <rongaudrey@gmail.com>
2023-10-06 22:31:58 -0700

    hahahhahahahahahaha

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+// Serve static files from the 'src' directory
+app.use(express.static(path.join(__dirname, 'src')));
+
+// GET request handler
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/pages/index.html'));
+});
+
+// POST request handler
+app.post('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/pages/submitted.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
